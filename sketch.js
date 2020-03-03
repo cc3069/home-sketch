75a1f918603ca3261a6ba2c279d9c49b790ba21a
cc3069 <61521909+cc3069@users.noreply.github.com>
2020-03-02 20:26:42 -0500

    I wanted to give the emphasis of my animated home to be similar to Monsters Inc.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,5 @@
-
-function setup() {
+function draw() {
     createCanvas(windowWidth, windowHeight);
-
 let x = 10
 let y = 10
 let width = 70
@@ -10,116 +8,66 @@ let radius = 20
 let side = 320
 let number = 4
 let number2 = 2.6
-let write = "LakeVille Lions"
 field(x,y,width,height)
 playarea1(x,y,side)
 playarea2(x,y,side)
 
 function field(x,y,width,height){
-fill('hsl(160, 100%, 50%)');
+background("black");
 strokeWeight(4);
-rect(x,y, width+1270, height+390);
+fill(0, 0, frameCount % 255);
+rect(2 * frameCount/90, 300, 50, 50);
+rect(2 * frameCount/90, 550, 50, 50);
+rect(2 * frameCount/90, 650, 50, 50);
+fill("white")
+ let e = 340 + 100 * cos(frameCount / 100);
+ let f =100 + 100 * sin(frameCount / 100);
+   ellipse (e, f, 50, 50);
+let g = 260 + 100 * cos(frameCount / 100);
+ let i = 380 + 100 * sin(frameCount / 100);
+     ellipse (g, i, 50, 50);
+let z = 1018 + 100 * cos(frameCount / 100);
+let w = 100 + 100 * sin(frameCount / 100);
+         ellipse (z, w, 50, 50);
+let k = 860 + 100 * cos(frameCount / 100);
+let s = 380 + 100 * sin(frameCount / 100);
+         ellipse (k, s, 50, 50);
 //storage
-stroke("white");
-strokeWeight(2.6);
 rectMode(CENTER);
-fill ("rgb(178,34,34)")
-rect(x+640, y+113, width+500, height+40);
-//roof
-fill("brown");
-strokeWeight(2.6);
-stroke("white")
-rect(x+640, y+30, width+430, height);
+fill(0, 0, frameCount % 255);
+rect(x+640, 23 + 100 * sin(frameCount / 100), width+500, height+40);
+rect(x+640, 40 * sin(frameCount / 100), width+430, height);
 //knob
-fill("white");
-rect(x+640, y+128, width+80, height+10);
 fill("black");
+rect(x+640,y+128, width+80, height+10);
+fill("white");
 noStroke();
 circle(x+640, y+128, radius);
-// left tree1
-strokeWeight(4);
-stroke("black");		
-fill("brown");
-rect(x+185, y+190, width-50, height+40);
-strokeWeight(4);
-stroke("black");	
-strokeWeight(4);
-stroke("black");	
-fill ('hsb(160, 100%, 50%)');	
- ellipse(x+195, y+165, width, height+10);
-// left tree2
-strokeWeight(4);
-stroke("black");	
- fill("brown");
- rect(x+285, y+140, width-50, height+40);
-strokeWeight(4);
-stroke("black");	
-fill ('hsb(160, 100%, 50%)')
- ellipse(x+295, y+105, width, height+50);
-//right tree1
-strokeWeight(4);
-stroke("black");
-fill("brown");
-rect(x+1105, y+190, width-50, height+40);
-strokeWeight(4);
-stroke("black");	
-fill ('hsb(160, 100%, 50%)');
-ellipse(x+1105, y+125, width, height+50);
-//right tree2
-strokeWeight(4);
-stroke("black");
-fill("brown");
-rect(x+1015, y+120, width-50, height+40);
-strokeWeight(4);
-stroke("black");
-fill ('hsb(160, 100%, 50%)');
-ellipse(x+1015, y+90, width, height+10);
+circle( x+640, y + 628, radius)
+
+	
 //White Balls
 strokeWeight(4);
 stroke("black");
-fill("white");
-circle(x+240, y+360, radius);
-circle(x+840, y+340, radius);
-circle(x+240, y+290, radius);	
-circle(x+840, y+400, radius);	
-//cone
-strokeWeight(4);
-stroke("black");
-fill("orange");	
-rect(x+370, y+290, width-20, height+40);
-rect(x+740, y+270, width-20, height+40);
-ellipse(x+370,y+350, width+25, height+15);
-ellipse(x+740, y+340, width+25, height+15)
+fill(0, 0, frameCount % 255);
+circle(650, 238, 20)
+circle(650, 338, 20)
+circle(650, 438, 20)
+circle(650, 738, 20)
 }
+
 	
 function playarea1(x,y,side){
-fill("grey");
-strokeWeight(4);
-stroke("blue");
-square(x+1190,y+545, side)
-stroke("black");
-fill("purple")
-ellipse(x+1188, y+550, width+153, height+130);
-stroke("white");
-fill("blue")
-ellipse(x+1188, y+550, width+80, height+40);
-textSize(16);
-text(write, x+1140, y+550);
+fill(0, 0, frameCount % 255);
+let h = 1198 + 100 * cos(frameCount / 100);
+let w = 560 + 100 * sin(frameCount / 100);
+    ellipse(h, w, 153, 100);
 }
 
 function playarea2(x,y,side){
-fill("grey");
-strokeWeight(4);
-stroke("blue");
-square(x+160, y+545, side)
-stroke("black");
-fill("purple")
-ellipse(x+140,y+550, width+153, height+140);
-stroke("white");
-fill("blue")
-ellipse(x+140, y+550, width+83, height+40);
-textSize(16);
-text(write,x+93, y+550);
-    }
+fill(0, 0, frameCount % 255);
+let a = 150 + 100 * cos(frameCount / 100);
+let b = 560 + 100 * sin(frameCount / 100);
+   ellipse(a, b, 153, 100);
 }
-
+}
